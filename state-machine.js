92f0ba3f95060d850e5ea7517fa7ff5b3bc5e43a
m7nsoft <m7nsoft@gmail.com>
2012-12-20 19:39:46 +0100

    W.I.P. regex events and froms, etc...

--- state-machine.js
+++ state-machine.js
@@ -1,3 +1,11 @@
+//hey, hey, hey! I made some mods to this, but I don't like them. Instead of a new states array, what a proper (imo) impl should have is:
+// - change events cfg prop to 'transitions', and field 'name' to 'event' (backwards compatibility would be ok)
+// - allow regex definiton on event and from fields (maybe event_re and from_re to avoid problems) to ease default behaviors definition avoiding explicit listing of all valid and
+//   usually similar event-state combinations.
+//   · to impl this, we probably need a first pass on trasitions array to grab all event and state names, thus avoiding problems while processing regex that match
+//     events or states defined in future entries.
+//   · regex should have less priority than explicit definitions to impl default behaviours (i.e. go to some state when an event not explicitly defined for this source state occurs)
+
 (function (window) {
 
   var StateMachine = {
@@ -28,27 +36,269 @@
 
     create: function(cfg, target) {
 
-      var initial   = (typeof cfg.initial == 'string') ? { state: cfg.initial } : cfg.initial; // allow for a simple string, or an object with { state: 'foo', event: 'setup', defer: true|false }
+      var initial;
       var fsm       = target || cfg.target  || {};
-      var events    = cfg.events || [];
+      var raw_definitions = cfg.transitions || cfg.events || []; // { event: 'str' | event_regex: 'str', from from_regex, to}
       var callbacks = cfg.callbacks || {};
-      var map       = {};
+      var map       = {}; // stores the active destination state for a given event-from pair
+      var defMap    = {};  // same structure than map, but instead of storing the destination state, stores an array with the attributes of all the transition definitions that define a destination for a given event-from pair
       var graph     = cfg.graph; // if cfg.graph is defined, create() adds to it a string member named 'DOTSource' containing the state machine's graph in DOT language
 
-      var add = function(e) {
-        var from = (e.from instanceof Array) ? e.from : (e.from ? [e.from] : [StateMachine.WILDCARD]); // allow 'wildcard' transition if 'from' is not specified
-        map[e.name] = map[e.name] || {};
-        for (var n = 0 ; n < from.length ; n++)
-          map[e.name][from[n]] = e.to || from[n]; // allow no-op transition if 'to' is not specified
+      var events = []; // collected event names for regex expansion
+      var states = []; // collected state names for regex expansion
+      var definitions; // preprocessed transition definitions array (regex strings and wildcard values replaced by RegExp instances, single strings wrapped in an array, etc.)
+
+      /* ============= create's helper functions ========== */
+
+      // processes xxx/xxx_regex field pairs an returns either an array or a RegExp instance
+      var processFields = function(value, regex) {
+        if (value && regex)
+          return {error: "Specify explicit values or a regex, not both"};
+
+        var wildcard_regex = new RegExp('.*');
+        var ret;
+
+        if (!value && !regex) {
+          ret = wildcard_regex; // backwards compat for missing 'from'. Note that this will also be applied for missing 'event' and 'event_regex'.
+        } else if (value) {
+          var val = (value instanceof Array) ? value : [value];
+          var has_wildcard = val.some(function(elem) { return elem === StateMachine.WILDCARD; });
+          if (has_wildcard) {
+            if (val.length === 1) {
+              ret = wildcard_regex;
+            } else {
+              //XXX mixed values and wildcards... I would throw an error here. However, backwards compatibility wins
+              ret = wildcard_regex;
+            }
+          } else {
+            ret = val;
+          }
+        } else if (regex) {
+          ret = new RegExp(regex);
+        } else {
+          throw "ASSERT: We shouldn't reach this point!!!";
+        }
+
+        return ret;
       };
 
-      if (initial) {
-        initial.event = initial.event || 'startup';
-        add({ name: initial.event, from: 'none', to: initial.state });
+      var collectNames = function(def) { // definitions should already be preprocessed and checked
+        if (!(def.event instanceof RegExp)) {
+          events = events.concat(def.event);
+        }
+        if (!(def.from instanceof RegExp)) {
+          states = states.concat(def.from);
+        }
+        if (def.to) {
+          states.push(def.to);
+        }
+      };
+
+      var getActiveDef = function(defList) {
+        var actives = defList.filter(function(def) { return def.active; });
+        if (actives.length !== 1) {
+          throw "ASSERT: More than one active definition?!!";
+        }
+        return actives[0];
+      };
+
+      // Given two definitions of the same transition, decides which has higher priority.
+      // Currently, the only rule is that a regex has less priority than a explicit value.
+      var getHigherPriorityDef = function(a, b) {
+        var e1 = {error: "All regex"};  //TODO meaningful messages, please
+        var e2 = {error: "Mixed stuff"};
+        var e3 = {error: "All explicit"};
+
+        // Defines the output for all the combinations of {a, b}.{is_event_regex, is_from_regex} 
+        // 0 = regex, 1 = EXPLICIT
+        // a.e a.f b.e b.f (msb to lsb)
+        var table = [
+          e1, b,  b,  b,
+          a,  e2, e2, b,
+          a,  e2, e2, b,
+          a,  a,  a,  e3
+        ];
+
+        if ( typeof a.is_event_regex !== 'boolean'
+          || typeof a.is_from_regex  !== 'boolean'
+          || typeof b.is_event_regex !== 'boolean'
+          || typeof b.is_from_regex  !== 'boolean')
+        {
+          throw "ASSERT: Something is wrong in regex flags";
+        }
+
+        // note that table is defined with value 0 for regex
+        var idx = !a.is_event_regex << 3 | !a.is_from_regex << 2 | !b.is_event_regex << 1 | !b.is_from_regex;
+
+        return table[idx];
+      };
+
+      var addDefinition = function(def) {
+
+        // expand event regex if necessary
+        var eventList;
+        var is_event_regex = false;
+        if (def.event instanceof RegExp) {
+          is_event_regex = true;
+          eventList = events.filter(function(e) { return def.event.test(e); });
+        } else {
+          eventList = def.event;
+        }
+
+        // expand from regex if necessary
+        var froms;
+        var is_from_regex = false;
+        if (def.from instanceof RegExp) {
+          is_from_regex = true;
+          froms = states.filter(function(s) { return def.from.test(s); });
+        } else {
+          froms = def.from;
+        }
+
+        // add the transition for each event and from (checking conflicts, etc.)
+        eventList.forEach(function(e) {
+          map[e] = map[e] || {};
+          defMap[e] = defMap[e] || {};
+
+          froms.forEach(function(from) {
+            var currentDef = {};
+            currentDef.to = def.to;
+            currentDef.idx = def.idx;
+            currentDef.is_event_regex = is_event_regex;
+            currentDef.is_from_regex  = is_from_regex;
+            currentDef.active = false;
+
+            if (map[e][from]) {
+              var activeDef = getActiveDef(defMap[e][from]);
+
+              if (map[e][from] !== def.to) { // conflict: different targets for the same transition
+                var winner = getHigherPriorityDef(activeDef, def);
+                if (winner.error) {
+                  throw "Conflicting transition definitions (" + activeDef.idx + " and " + def.idx + ") for event '" + e + "' and from '" + from + "': " + winner.error;
+                }
+
+                if (winner === def) {
+                  map[e][from] = def.to;
+                  activeDef.active = false;
+                  def.active = true;
+                }
+              }
+
+              defMap[e][from].push(def);
+            } else { // first transition definition for these event and from
+              def.active = true;
+              map[e][from] = def.to || from;
+              defMap[e][from] = [def];
+            }
+          });
+        });
+      };
+
+      // XXX if we want/need to generate fancier graphs may be worth it to look for a graphviz generation lib
+      //     or to write our own helper methods, i.e. printEdge(to, from, {label: xxx, color: ...})
+      var exportGraphviz = function(params) {
+        var graph = '';
+        var params = params || {};
+
+        var wline = function(str){
+          graph += str + '\n';
+        };
+        var color = function(e, f) {
+          var c;
+          if (e && f) {
+            c = 'purple';
+          } else if (e && !f) {
+            c = 'red';
+          } else if (!e && f) {
+            c = 'blue';
+          } else if (!e && !f) {
+            c = 'black';
+          }
+          return c;
+        };
+
+        wline('digraph fsm {');
+        wline('  rankdir=LR;');
+        wline('  node [shape = circle];');
+
+        for (var event in map) {
+          if (map.hasOwnProperty(event)) {
+            var eventMap = map[event];
+            for (var from in eventMap) {
+              if (eventMap.hasOwnProperty(from)) {
+                var defs = defMap[event][from];
+                var def = getActiveDef(defs);
+
+                var defsListStr = '';
+                if (params.defsList) {
+                    var indexes = defs.map(function(def) { return def.idx; });
+                    var idxStr = indexes.length > 1 ? '[' + indexes + ']' : '';
+                    defsListStr = ' (t:' + def.idx + idxStr + ')';
+                }
+                
+                var colorStr = ''; 
+                if (params.colors) {
+                    colorStr = ', color = ' + color(def.is_event_regex, def.is_from_regex);
+                }
+
+                wline('  ' + from + ' -> ' + eventMap[from] + ' [ label = "' + event + defsListStr + '"' + colorStr + ' ];');
+              }
+            }
+          }
+        }
+
+        wline('}');
+        return graph;
+      };
+
+      /* ================ create starts here ============= */
+
+      if (cfg.initial) {
+        // allow for a simple string, or an object with { state: 'foo', event: 'setup', defer: true|false }
+        initial = (typeof cfg.initial == 'string') ? { state: cfg.initial } : cfg.initial;
+
+        // match initial format with definitions' entry format
+        initial.event = (initial.event instanceof Array) ? initial.event : (initial.event ? [initial.event] : ['startup']);
+        initial.from  = ['none'];
+        initial.to    = initial.state;
+
+        addDefinition(initial);
+
+        // TODO should we add initial 'event' and 'to' to the lists for regex expansion???
       }
 
-      for(var n = 0 ; n < events.length ; n++)
-        add(events[n]);
+      // preprocess transition definitions
+      definitions = raw_definitions.map(function(t, idx) {
+        var new_t = {};
+
+        new_t.to = t.to;
+        new_t.idx = idx;
+
+        var from = processFields(t.from, t.from_regex);
+        if (from.error) {
+          throw "Error in from/from_regex of [" + idx + "]: " + from.error;
+        }
+        new_t.from = from;
+
+        var event = processFields(t.event || t.name, t.event_regex);
+        if (event.error) {
+          throw "Error in event/event_regex of [" + idx + "]: " + event.error;
+        }
+        new_t.event = event;
+
+        return new_t;
+      });
+
+      // collect 'named' events and states
+      definitions.forEach(function(def){ collectNames(def); });
+
+      // remove duplicates from collected lists
+      events = events.filter(function(elem, pos, array) { return array.indexOf(elem) === pos; });
+      states = states.filter(function(elem, pos, array) { return array.indexOf(elem) === pos; });
+
+      // create the map. Regexs are expanded here
+      definitions.forEach(function(def) { addDefinition(def); });
+
 
       for(var name in map) {
         if (map.hasOwnProperty(name))
@@ -57,64 +307,16 @@
 
       for(var name in callbacks) {
         if (callbacks.hasOwnProperty(name))
-          fsm[name] = callbacks[name]
+          fsm[name] = callbacks[name];
       }
 
       fsm.current = 'none';
       fsm.is      = function(state) { return this.current == state; };
-      fsm.can     = function(event) { return !this.transition && (map[event].hasOwnProperty(this.current) || map[event].hasOwnProperty(StateMachine.WILDCARD)); }
+      fsm.can     = function(event) { return !this.transition && (map[event].hasOwnProperty(this.current)); };
       fsm.cannot  = function(event) { return !this.can(event); };
       fsm.error   = cfg.error || function(name, from, to, args, error, msg, e) { throw e || msg; }; // default behavior when something unexpected happens is to throw an exception, but caller can override this behavior if desired (see github issue #3 and #17)
 
       if (graph) {
-        var exportGraphviz = function() {
-          // Collect state names for wildcard expansion
-          var states = [];
-          for (var event in map) {
-            if (map.hasOwnProperty(event)) {
-              var eventMap = map[event];
-              for (var from in eventMap) {
-                if (eventMap.hasOwnProperty(from)) {
-                  states.push(from);
-                  states.push(eventMap[from]);
-                }
-              }
-            }
-          }
-          // remove duplicates and wildcards
-          states = states.filter(function(elem, pos, array) { return elem !== StateMachine.WILDCARD && array.indexOf(elem) === pos; });
-
-          // Generate graphviz source
-          var graph = '';
-          var wline = function(str){
-              graph += str + '\n';
-          };
-
-          wline('digraph fsm {');
-          wline('  rankdir=LR;');
-          wline('  node [shape = circle];');
-
-          for (var event in map) {
-            if (map.hasOwnProperty(event)) {
-              var eventMap = map[event];
-              for (var from in eventMap) {
-                if (eventMap.hasOwnProperty(from)) {
-                  if (from !== StateMachine.WILDCARD) {
-                    wline('  ' + from + ' -> ' + eventMap[from] + ' [ label = "' + event + '" ];');
-                  } else {
-                    states.forEach(function(state) {
-                      wline('  ' + state + ' -> ' + eventMap[from] + ' [ label = "' + event + '" ];');
-                    });
-                  }
-                }
-              }
-            }
-          }
-
-          wline('}');
-          return graph;
-        };
-
         graph.DOTSource = exportGraphviz();
       }
 
@@ -149,7 +351,7 @@
       return function() {
 
         var from  = this.current;
-        var to    = map[from] || map[StateMachine.WILDCARD] || from;
+        var to    = map[from];
         var args  = Array.prototype.slice.call(arguments); // turn arguments into pure array
 
         if (this.transition)
